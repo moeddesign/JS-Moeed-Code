d4c18b37d90e3aa78baa0c94aa0a70f957657697
Abdul Moeed <moeed4964@gmail.com>
2025-01-22 08:18:59 +0000

    Basic

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,19 @@
+"Use Strict"; // Treat all JS code as newer version
+
+// alert("Hello") 
+
+let name = "moeed" // String 
+let age = 12 // Number
+let isLoggedIn = false // Bollean
+let number =  203944959660 //   bigint use in large number 
+let state = null // standalone Valuse 
+// undefined =>  unsatandalone value
+// symbols  => unique
+
+
+//Object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object 
+
+
