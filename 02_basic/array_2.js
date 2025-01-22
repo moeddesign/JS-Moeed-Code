15cdcc621956d584470ec3efd76ef06c4f427861
Abdul Moeed <moeed4964@gmail.com>
2025-01-22 13:28:22 +0000

    Array 2

--- 02_basic/array_2.js
+++ 02_basic/array_2.js
@@ -9,7 +9,7 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+const all_new_heros = [...marvel_heros , ...dc_heros]
 
 // console.log(all_new_heros);
 
@@ -29,3 +29,4 @@ let score2 = 200
 let score3 = 300
 
 console.log(Array.of(score1, score2, score3));
+
