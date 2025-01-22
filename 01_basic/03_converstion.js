2f017cea8da6f67dd56202a9772a17b437a85468
Abdul Moeed <moeed4964@gmail.com>
2025-01-22 08:33:13 +0000

    Converstion

---
+++ 01_basic/03_converstion.js
@@ -0,0 +1,21 @@
+let score = null
+
+// console.log( score);
+ let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => ture ; 0 => false 
+// "moeed" => true
+// "" => false
+
+
