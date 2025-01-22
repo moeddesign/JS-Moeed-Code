8b4275679051def531ec34c4f5b74870855137f0
Abdul Moeed <moeed4964@gmail.com>
2025-01-22 12:00:22 +0000

    Math Num

---
+++ 01_basic/num_math.js
@@ -0,0 +1,43 @@
+const score = 400 
+// console.log(score);
+
+const balance = new Number(100)
+
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.9753
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000000 
+// console.log(hundreds.toLocaleString('en-PK'));
+// console.log(hundreds.Max_Value());
+
+/*
+  ********* Math **********
+*/
+
+// console.log(Math);
+// console.log(Math.round(-4));
+// console.log(Math.ceil(5.3));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,5,6,78,97));
+// console.log(Math.max(9,8,7,3322));
+
+console.log(Math.random());
+console.log((Math.random()*20) + 1);
+
+
+const min = 10
+const max = 20 
+
+ 
+console.log(Math.floor(Math.random() * (max - min + 1))+min);
+
+
+
+
+
