1e0b473bc8c5602d0c76e8cc75b7e4705bd68cbb
Abdul Moeed <moeed4964@gmail.com>
2025-01-22 10:35:15 +0000

    Code With Moeed

---
+++ 01_basic/05_dataType_summary.js
@@ -0,0 +1,37 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3 // Number
+
+const isLoggedIn = false // Boolean
+const outsideTemp = null //Null
+let userEmail; // undefined
+
+const id = Symbol('123') // Symbol
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n // BigInt 
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]; // Array
+let myObj = {  // Object
+    name: "hitesh", 
+    age: 22,
+}
+
+const myFunction = function(){ //  function
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
