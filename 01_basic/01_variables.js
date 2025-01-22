548bb702b8a43cb1462a6ab9ece1f062d406371a
Abdul Moeed <moeed4964@gmail.com>
2025-01-22 07:58:25 +0000

    Basic Code

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,23 @@
+const acccountId = 144553  // we can't change const value
+let accountEmail = "moeed@gmail.com" 
+var accountPassword = "12345" 
+accountCity ="Okara" 
+let accountState;
+
+
+
+
+// acccountId = 2 not Allowed
+
+accountEmail = "malik@gmail.com"
+accountPassword ="908766"
+accountCity = "Lahore"
+
+// console.log(acccountId);
+
+/*
+   Prefer not use var
+   because of issue in block and funtional scope
+*/
+
+console.table([acccountId , accountEmail ,accountPassword , accountCity ,accountState])
---
+++ 01_basic/test.js
@@ -0,0 +1,7 @@
+console.log("moeed")
+// console.log("malik");
+for (let i = 20 ; i  < 201 ; i = i+20) {
+     console.log(i);
+     
+    
+}
